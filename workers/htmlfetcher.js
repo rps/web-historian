44c8b54f7823516552a5389f3cf7bd90984ce66c
Hack Reactor Students <github@hackreactor.com>
2013-10-16 19:48:23 -0700

    Reads list of sites and saves sites to file.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,23 @@
-// eventually, you'll have some code here that uses the tested helpers 
+// eventually, you'll have some code here that uses the tested helpers
 // to actually download the urls you want to download.
+
+var path = require('path');
+
+var helpers = require('./lib/html-fetcher-helpers.js');
+
+var fetcher = function(siteList){
+  var urlArray = [];
+  var downloaddir = path.join(__dirname, "/../data/sites/"); // tests will need to override this.
+  siteList = siteList || path.join(__dirname, "/../data/sites.txt");
+
+  helpers.readUrls(siteList,function(site){
+    // console.log(site.split('\n'));
+    urlArray = site.split('\n');
+  });
+  // console.log(urlArray);
+
+
+  for(var i = 0; i<urlArray.length-1; i++){
+    helpers.downloadUrls(urlArray[i], downloaddir);
+  }
+}();
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,7 +1,21 @@
-exports.readUrls = function(filePath, cb){
+var fs = require('fs');
+var req = require('request');
+var moment = require('moment');
 
+exports.readUrls = function(filePath, cb){
+  var fileOutput = fs.readFileSync(filePath);
+  cb(fileOutput.toString());
 };
 
-exports.downloadUrls = function(urls){
-  // fixme
+exports.downloadUrls = function(url, sitesdir){
+  req('http://'+url, function (error, response, body) {
+    console.log(error)
+
+    if (!error && response.statusCode === 200) {
+
+      var fileName = sitesdir+url+moment().format("_MMM_Do_mm:ss");
+      console.log('filename',fileName);
+      fs.writeFileSync(fileName,body);
+    }
+  });
 };
